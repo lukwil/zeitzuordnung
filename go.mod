a158109b5c3318855b1d253312cdf868b0c37579
Lukas Willburger <lukas.willburger@fim-rc.de>
2022-02-15 13:57:58 +0100

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lukwil/zeitzuordnung
+
+go 1.15
---
+++ zeitzuordnung.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+var records = [][]string{
+	{"Datum", "Start", "Ende", "Pause"},
+}
+
+func main() {
+	timezone, _ := time.LoadLocation("Europe/Berlin")
+	today := time.Now().In(timezone)
+
+	year, week := today.ISOWeek()
+	fileName := fmt.Sprintf("zeitzuordnung-%v-KW%v.csv", year, week)
+	f, err := os.Create(fileName)
+	defer f.Close()
+	if err != nil {
+		log.Fatalln("failed to open file", err)
+	}
+
+	generateDataForWeek(today)
+
+	w := csv.NewWriter(f)
+	w.Comma = ';'
+	defer w.Flush()
+
+	for _, record := range records {
+		if err := w.Write(record); err != nil {
+			log.Fatalln("error writing record to file", err)
+		}
+	}
+}
+
+func generateDataForWeek(date time.Time) {
+	// going back from current day until monday
+	startTime := "09:00"
+	endTime := "18:00"
+	breakTime := "01:00"
+	for date.Weekday() != time.Monday {
+		fmt.Println("Runter")
+		appendEntry(date, startTime, endTime, breakTime)
+
+		date = date.AddDate(0, 0, -1)
+	}
+
+	// One hour less for monday --> 39h/week
+	endTime = "17:00"
+	appendEntry(date, startTime, endTime, breakTime)
+}
+
+func appendEntry(date time.Time, startTime, endTime, breakTime string) {
+	records = append(records, []string{
+		date.Format("02.01.2006"),
+		startTime,
+		endTime,
+		breakTime,
+	})
+}
